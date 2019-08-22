ba568f28275460bcc184911a076c059e174d2c05
Odaawa <odaawa77@gmail.com>
2019-08-22 15:40:15 +0100

    Commit Hackathon files

---
+++ Hackathon - Pair/formpage.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+
+button.onclick =function(){
+ 	//prompt kinda works similar to alert
+ 	let Submit = alert('Thanks for signing up!');
+ 	
+ }
+
+ 
